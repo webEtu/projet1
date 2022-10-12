0e953fe376dbce2b6a2126a4cb10a6b26c392014
webEtu <azadeh.ghasemizadeh2022@campus-eni.fr>
2022-10-12 11:40:32 +0200

    DataTypes

---
+++ src/DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int age=44;
+        String nom="Lisangola";
+        String prenom="Christian";
+        double poids=80.3;
+        System.out.println("Nom:" + nom);
+        System.out.println("Prenom:" + prenom);
+        System.out.println("Age:" + age);
+        System.out.println("Poid:" + poids);
+    }
+}
