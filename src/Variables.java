0a12db60252b7becbbb07e617c3751c40fb55309
webEtu <azadeh.ghasemizadeh2022@campus-eni.fr>
2022-10-12 11:34:11 +0200

    les variables

---
+++ src/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        int longueur=500;
+        int largeur=250;
+        int perimetre=2*(longueur+largeur);
+        int surface=longueur*largeur;
+        System.out.println(perimetre);
+        System.out.println(surface);
+    }
+}
